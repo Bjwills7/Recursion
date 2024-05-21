a4dbb72bca369e270a922f0c448f8b809832b744
bjwills7 <bjwills7@hotmail.com>
2024-05-20 20:57:04 -0500

    Add iterative and recursive versions of a function that returns an array of the Fibonacci sequence with a length that matches the int parameter passed to it

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,11 +1,26 @@
-function fibonacci(n) {
-    if (n === 0) {
-        return 0;
-    } else {
-        return n + fibonacci(n - 1);
+function iterativeFib(n) {
+    const arr = [];
+
+    if (n > 0) {
+        for (let i = 0; i < n; i++) {
+            if (i < 2) arr.push(i);
+            else arr.push(arr[i - 2] + arr[i - 1]);
+        }
+    }
+
+    return arr;
+}
+
+function recursiveFib(n) {
+    if (n < 1) return []; 
+    else if (n === 1) return [0];
+    else if (n === 2) return [0, 1];
+    else {
+        var arr = recursiveFib(n - 1);
+        arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+        return arr;
     }
 }
 
-function fib(n) {
-    n ? n + fib(n-1) : n;
-}
+console.log(iterativeFib(8));
+console.log(recursiveFib(8));
